d270cae77c094283657965a43e139fa003989af6
fankunpeng <2361598538@qq.com>
2020-06-12 21:04:02 +0800

    第一次可用

--- exeFeedback.m
+++ exeFeedback.m
@@ -26,7 +26,7 @@ N0 = 1.4e24;
 tauP = 1.927e-12; % photon lifetime [s]
 tauS = 2.04e-9; % carrier lifetime [s]
 alpha = 3.0; % alpha parameter
-Nth = 0.0
+Nth = 0.0;
 Jth = 0.0;
 J = 0.0;
 kap = 0.0;
@@ -50,14 +50,14 @@ omega0 = 2.0 * pi * C / lambda;
 phaseShift = rem(omega0 * tau, 2.0 * pi);
 % initial phase shift
 delayNum = floor(tau / h);
-delayIndex = 0;
+delayIndex = 1;
 DELAY_MAX = 100000;
 eDelay = zeros(1, DELAY_MAX);
 phiDelay = zeros(1, DELAY_MAX);
 
 for i = 1 : DELAY_MAX
-    eDelay(i) = a(0);
-    phiDelay(i) = a(1);
+    eDelay(i) = a(1);
+    phiDelay(i) = a(2);
 end
 
 for i = 1 : trans
@@ -65,59 +65,67 @@ for i = 1 : trans
     % rungakota
     x = zeros(1, M);
     b = zeros(4, M);
-    theta = rem(phaseShift + a(1) - phiDelay(delayIndex), 2.0 * pi);
+    theta = rem(phaseShift + a(2) - phiDelay(delayIndex), 2.0 * pi);
     for i = 1 : 4
         for j = 1 : M
             if i == 1
                 x(j) = a(j);
             elseif i == 2
-                x(j) = a(j) + h * b(1)(j) / 2.0;
+                x(j) = a(j) + h * b(1,j) / 2.0;
             elseif i == 3
-                x(j) = a(j) + h * b(2)(j) / 2.0;
+                x(j) = a(j) + h * b(2,j) / 2.0;
             elseif i == 4
-                x(j) = a(j) + h * b(3)(j) / 2.0;
+                x(j) = a(j) + h * b(3,j) / 2.0;
             end
-            lsb = b(i);
-            lsb(1) = 1.0 / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) * x(1)+ kap * eDelay(delayIndex) * cos(theta);
-            lsb(2) = alpha / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) - kap * eDelay(delayIndex) / x(1) * sin(theta);
-            lsb(3) = J - x(3) / tauS - Gn * (x(3) - N0) * x(1) * x(1);
+            b(i,1) = 1.0 / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) * x(1)+ kap * eDelay(delayIndex) * cos(theta);
+            b(i,2) = alpha / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) - kap * eDelay(delayIndex) / x(1) * sin(theta);
+            b(i,3) = J - x(3) / tauS - Gn * (x(3) - N0) * x(1) * x(1);
         end
     end
+    break
     for i = 1 : M
-        a(i) += h * (b(1)(i) + 2.0 * b(2)(i) + 2.0 * b(3)(i) + b(4)(i)) / 6.0;
+        a(i) = a(i) + h * (b(1,i) + 2.0 * b(2,i) + 2.0 * b(3,i) + b(4,i)) / 6.0;
     end
-    eDelay(delayIndex) = a(0);
-    phiDelay(delayIndex) = a(1);
-    delayIndex = rem((delayIndex + 1), delayNum);
+    eDelay(delayIndex) = a(1);
+    phiDelay(delayIndex) = a(2);
+    delayIndex = delayIndex + 1;
 end
+
+arrI = [];
+arrA = [];
 
 for i = 1 : n
     t = h * (trans + i);
     % output i % div == 0
+    o1 = h * i * 1e9;
+    o2 = a(1) * a(1) * 1e-20;
+    arrI(i) = o1;
+    arrA(i) = o2;
+    
     x = zeros(1, M);
     b = zeros(4, M);
-    theta = rem(phaseShift + a(1) - phiDelay(delayIndex), 2.0 * pi);
+    theta = rem(phaseShift + a(2) - phiDelay(delayIndex), 2.0 * pi);
     for i = 1 : 4
         for j = 1 : M
             if i == 1
                 x(j) = a(j);
             elseif i == 2
-                x(j) = a(j) + h * b(1)(j) / 2.0;
+                x(j) = a(j) + h * b(1,j) / 2.0;
             elseif i == 3
-                x(j) = a(j) + h * b(2)(j) / 2.0;
+                x(j) = a(j) + h * b(2,j) / 2.0;
             elseif i == 4
-                x(j) = a(j) + h * b(3)(j) / 2.0;
+                x(j) = a(j) + h * b(3,j) / 2.0;
             end
-            lsb = b(i);
-            lsb(1) = 1.0 / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) * x(1)+ kap * eDelay(delayIndex) * cos(theta);
-            lsb(2) = alpha / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) - kap * eDelay(delayIndex) / x(1) * sin(theta);
-            lsb(3) = J - x(3) / tauS - Gn * (x(3) - N0) * x(1) * x(1);
+            b(i,1) = 1.0 / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) * x(1)+ kap * eDelay(delayIndex) * cos(theta);
+            b(i,2) = alpha / 2.0 * (Gn * (x(3) - N0) - 1.0 / tauP) - kap * eDelay(delayIndex) / x(1) * sin(theta);
+            b(i,3) = J - x(3) / tauS - Gn * (x(3) - N0) * x(1) * x(1);
         end
-    end
     for i = 1 : M
-        a(i) += h * (b(1)(i) + 2.0 * b(2)(i) + 2.0 * b(3)(i) + b(4)(i)) / 6.0;
+        a(i) = a(i) + h * (b(1,i) + 2.0 * b(2,i) + 2.0 * b(3,i) + b(4,i)) / 6.0;
     end
-    eDelay(delayIndex) = a(0);
-    phiDelay(delayIndex) = a(1);
-    delayIndex = rem((delayIndex + 1), delayNum);
+    eDelay(delayIndex) = a(1);
+    phiDelay(delayIndex) = a(2);
+    delayIndex = delayIndex + 1;
+    end
 end
+plot(arrI, arrA);
